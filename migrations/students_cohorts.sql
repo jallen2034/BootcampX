1ac1c946f3337478787bf08357e402009d41c7e0
Jacob Allen <jallen2033@gmail.com>
2021-02-02 01:56:10 +0000

    added assignment aand submission data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS cohorts;
+DROP TABLE IF EXISTS assignment;
+DROP TABLE IF EXISTS assignment_submission;
 
+/* create cohorts table */
 CREATE TABLE cohorts (
   id serial PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -8,6 +11,7 @@ CREATE TABLE cohorts (
   end_date DATE
 );
 
+/* create students table */
 CREATE TABLE students (
   id serial PRIMARY KEY NOT NULL,
   name VARCHAR(255),
@@ -17,4 +21,23 @@ CREATE TABLE students (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+/* create assingnments table */
+CREATE TABLE assignments (
+  id serial PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+/* create assignments table which references assignment and students tables */
+CREATE TABLE assignment_submissions (
+  id serial PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
 );
